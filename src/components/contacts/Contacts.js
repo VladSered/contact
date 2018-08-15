25674d4badc3a1568321d0146fd022fab768d30b
Vladislav <vladislav08011993@outlook.com>
2018-08-15 21:22:38 +0300

    commit

---
+++ src/components/contacts/Contacts.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import Contact from "./Contact";
+import { Consumer } from "../../context";
+
+class Contacts extends Component {
+  render() {
+    return (
+      <Consumer>
+        {value => {
+          return (
+            <React.Fragment>
+              <h5 className="display-6 mb-4">Contact List</h5>
+              {value.contacts.map(contact => (
+                <Contact key={contact.id} contact={contact} />
+              ))}
+            </React.Fragment>
+          );
+        }}
+      </Consumer>
+    );
+  }
+}
+export default Contacts;
